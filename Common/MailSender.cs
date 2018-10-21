16027df2e23bf46a61e4b80c63d85c74bd3e5811
MWadolowski <michwadol@gmail.com>
2018-10-21 23:33:04 +0200

    dobra tam, bo to musi być projekt .net standard a nie .net core

---
+++ Common/MailSender.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+
+namespace Models
+{
+    public class MailSender
+    {
+        public void Send(string to, string body, string subject, IList<Attachment> attachments = null)
+        {
+            var email = new MailMessage
+            {
+                To = { to },
+                From = new MailAddress("DC.jazda@gmail.com"),
+
+                Body = body,
+                Subject = subject
+            };
+            foreach (var attachment in attachments ?? new List<Attachment>()) email.Attachments.Add(attachment);
+
+            SmtpClient client = new SmtpClient
+            {
+                Port = 25,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Host = "in-v3.mailjet.com",
+                Credentials = new NetworkCredential("b9e3cd0a28a7be86c57ba26dcad4aa08", "6b104384465d5c4a7aee4ab489a53ac5"),
+            };
+
+            client.Send(email);
+        }
+    }
+}
---
+++ Common/ProductData.cs
@@ -0,0 +1,8 @@
+﻿namespace Models
+{
+    public class ProductData
+    {
+        public string Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
